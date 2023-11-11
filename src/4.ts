51f9d797cc9438b434c18f51f4dabeebb077c087
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-11-11 07:03:09 +0200

    fix HW

--- src/4.ts
+++ src/4.ts
@@ -1,68 +1,48 @@
+abstract class House {
+  public door: boolean;
+  public key: Key;
+  public tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Open");
+    }
+  }
+}
 class Key {
-    private signature: number;
-  
-    constructor() {
-      this.signature = Math.random();
-    }
-  
-    getSignature(): number {
-      return this.signature;
-    }
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
   }
-  
-  class Person {
-    private key: Key;
-  
-    constructor(key: Key) {
-      this.key = key;
-    }
-  
-    getKey(): Key {
-      return this.key;
-    }
+  getSignature(): number {
+    return this.signature;
   }
-  
-  abstract class House {
-    door: boolean = false;
-    key: Key;
-    tenants: Person[] = [];
-  
-    constructor(key: Key) {
-      this.key = key;
-    }
-  
-    comeIn(person: Person): void {
-      if (this.door) {
-        this.tenants.push(person);
-        console.log('Person entered the house.');
-      } else {
-        console.log('Door is closed. Person cannot enter.');
-      }
-    }
-  
-    abstract openDoor(key: Key): void;
+}
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
   }
-  
-  class MyHouse extends House {
-    constructor(key: Key) {
-      super(key);
-    }
-  
-    openDoor(key: Key): void {
-      if (key.getSignature() === this.key.getSignature()) {
-        this.door = true;
-        console.log('Door opened.');
-      } else {
-        console.log('Wrong key. Door remains closed.');
-      }
-    }
-  }
-  
-  // Тестування
-  const key = new Key();
-  const house = new MyHouse(key);
-  const person = new Person(key);
-  
-  house.openDoor(person.getKey()); 
-  house.comeIn(person); 
-  
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
